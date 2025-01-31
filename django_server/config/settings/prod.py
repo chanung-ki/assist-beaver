bb7205450dd787ef7b815ad2456e053ee7808fc5
chanung-ki <117616574+chanung-ki@users.noreply.github.com>
2025-01-31 22:44:40 +0900

    chore: 배포를 위한 설정

--- django_server/config/settings/prod.py
+++ django_server/config/settings/prod.py
@@ -2,9 +2,7 @@
 
 from .base import *
 
-# 원래는 DEBUG = False가 맞으나 이때 EC2 초반에 테스트 목적으로 True로 설정
-# DEBUG = False
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ 
 	'52.79.140.89',
@@ -12,6 +10,4 @@ ALLOWED_HOSTS = [
 
 STATIC_URL = 'static/'
 
-# STATIC_ROOT를 써야 하지만 마찬가지로 테스트를 위해 STATICFILES_DIRS 사용
-# STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [BASE_DIR / 'static',]
+STATIC_ROOT = BASE_DIR / 'static'
